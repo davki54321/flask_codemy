45a2ef77dcc5db997edf602c7e079ee5247f42ed
David Kim <4dkim5@gmail.com>
2023-02-21 21:56:39 +0100

    name form

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+# Create form class
+class NameForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create flask instance
 # This helps flask find files in directory
 app = Flask(__name__)
+# Creates secret key
+app.config['SECRET_KEY'] = "somethingsomething"
 
 # Create a route decorator
 @app.route('/')
@@ -29,3 +41,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Name form
+@app.route('/name_form', methods=["GET", "POST"])
+def name_form():
+    name = None
+    form = NameForm()
+    # Validates form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+
+    return render_template("name_form.html",
+                            name = name,
+                            form = form)
